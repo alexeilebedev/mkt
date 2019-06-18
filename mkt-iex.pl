ec996affa73a2d648868779f1f2fdf27a04ae838
Alexei Lebedev <alexei@lebe.dev>
2019-06-18 10:50:50 -0400

    add iex cloud api support

--- mkt-iex.pl
+++ mkt-iex.pl
@@ -1,4 +1,5 @@
 #!/usr/local/binperl
+my $iextoken;
 
 #-------------------------------------------------------------------------------
 # convert IEX timestamp to ISO 8601 form
@@ -18,10 +19,27 @@ sub iex_begin_fetch($) {
 
 #-------------------------------------------------------------------------------
 
+sub iex_load_token() {
+    if (!-f "data/iextoken") {
+        print STDERR "file data/iextoken doesn't seem to exist. please register for an IEX cloud account\n"
+            ."(it should be free) and place the secret token (starting with sk_) into this file.\n"
+            ."Re-run mkt as 'VERBOSE=1 mkt' to see if the token correctly appears in the curl request\n";
+    } else {
+        open my $R, "<data/iextoken";
+        $iextoken=<$R>;
+        chomp $iextoken;
+    }
+}
+
+#-------------------------------------------------------------------------------
+
 sub iex_end_fetch {
+    if (!$iextoken) {
+        iex_load_token();
+    }
     if (scalar(@iexsyms)>0){
 	my $symbol = join(",",@iexsyms);
-	my $obj=fetch_json("https://ws-api.iextrading.com/1.0/stock/market/batch?symbols=$symbol&types=quote&range=1m");
+	my $obj=fetch_json("https://cloud.iexapis.com/v1/stock/market/batch?symbols=$symbol&types=quote&range=15m&token=$iextoken");
 	foreach my $symbol(sort keys %$obj){
 	    if ($obj->{$symbol}){
 		my $quote=$obj->{$symbol}->{quote};
